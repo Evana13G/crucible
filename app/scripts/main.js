b1cff6c34f2c49fcb344d9660e6096edc47079b7
Evana13G <Evana13G@gmail.com>
2014-12-19 15:01:58 -0500

    added conformace metadata code.... review.html

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,33 +1,34 @@
 'use strict';
 
 $(document).ready(function(){
+// 	$('#conformance-metadata').hide();
+// 	$('#rest-metadata').hide();
+// 	$('#document-metadata').hide();
+// 	$('#messaging-metadata').hide();
 
-	$('#conformance-metadata').hide();
-	$('#rest-metadata').hide();
-	$('#document-metadata').hide();
-	$('#messaging-metadata').hide();
-
-	$('#metadata-selector').change(function(){
-		$('#conformance-metadata').hide();
-		$('#rest-metadata').hide();
-		$('#document-metadata').hide();
-		$('#messaging-metadata').hide();
-		var value = $('#metadata-selector').val().toLowerCase();
-		var targetString = '#' + value.split(' ')[0] + '-metadata';
-		$(''+targetString).show();
-	});
+// 	$('#metadata-selector').change(function(){
+// 		$('#conformance-metadata').hide();
+// 		$('#rest-metadata').hide();
+// 		$('#document-metadata').hide();
+// 		$('#messaging-metadata').hide();
+// 		var value = $('#metadata-selector').val().toLowerCase();
+// 		var targetString = '#' + value.split(' ')[0] + '-metadata';
+// 		$(''+targetString).show();
+// 	});
 
 	// initialize table
-	$('.report-table > tbody > tr').removeClass('active');
-	$('.report-table > tbody > tr').first().addClass('active');
-	var initialDialogue = $('.report-table > tbody > tr').children()[1].innerText;
-	$('#test-dialogue').text(initialDialogue);
+	// $('.report-table > tbody > tr').removeClass('active');
+	// $('.report-table > tbody > tr').first().addClass('active');
+	// var initialDialogue = $('.report-table > tbody > tr').children()[1].innerText;
+	// $('#test-dialogue').text(initialDialogue);
 
-	$('.report-table > tbody > tr ').click(function(){
-		$('.report-table > tbody > tr').removeClass('active');
-		$(this).addClass('active');
-		var text1 = $(this).children()[1].innerText;
-	    $('#test-dialogue').text(text1);
-	});
+	// $('.report-table > tbody > tr ').click(function(){
+	// 	$('.report-table > tbody > tr').removeClass('active');
+	// 	$(this).addClass('active');
+	// 	var text1 = $(this).children()[1].innerText;
+	//     $('#test-dialogue').text(text1);
+	// });
 
+
+	
 });
